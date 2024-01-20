d34a07dc17b699b8f23ae29271cb690c8084c19b
aqm1970 <aqmontgomery@gmail.com>
2024-01-20 14:03:54 -0500

    Use FlickApi

--- app/src/main/java/com/bignerdranch/android/photogallery/app/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/app/FlickrApi.kt
@@ -3,6 +3,12 @@ package com.bignerdranch.android.photogallery.app
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=e0fa3dcb3fb1813a7332b89bb1c884f8" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): String
 }
