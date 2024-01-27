04693f70bdad3c7f07c64f15716fb82c0fb31a44
aqm1970 <aqmontgomery@gmail.com>
2024-01-27 13:40:46 -0500

    Defining UI State

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -17,16 +18,23 @@ class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
     private val preferenceRepository = PreferenceRepository.get()
 
-    private val _galleryItems: MutableStateFlow<List<GalleryItem>> = MutableStateFlow(emptyList())
-    val galleryItems: StateFlow<List<GalleryItem>>
-        get() = _galleryItems.asStateFlow()
+    private val _uiState: MutableStateFlow<PhotoGalleryUiState> =
+        MutableStateFlow(PhotoGalleryUiState())
+    val uiState: StateFlow<PhotoGalleryUiState>
+        get() = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
             preferenceRepository.storedQuery.collectLatest { storedQuery ->
                 try {
                     val items = fetchGalleryItems(storedQuery)
-                    _galleryItems.value = items
+
+                    _uiState.update { oldState ->
+                        oldState.copy(
+                            images = items,
+                            query = storedQuery
+                        )
+                    }
                 } catch (ex: Exception) {
                     Log.e(TAG, "Failed to fetch gallery items", ex)
                 }
@@ -45,4 +53,9 @@ class PhotoGalleryViewModel : ViewModel() {
             photoRepository.fetchPhotos()
         }
     }
+
+    data class PhotoGalleryUiState(
+        val images: List<GalleryItem> = listOf(),
+        val query: String = "",
+    )
 }
