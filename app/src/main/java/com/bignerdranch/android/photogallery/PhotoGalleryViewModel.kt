b7f2534a59af8a6c117c429035c93dcd865bcc7c
aqm1970 <aqmontgomery@gmail.com>
2024-01-27 13:22:02 -0500

    Simple Persistence with DataStore

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -7,6 +7,7 @@ import com.bignerdranch.android.photogallery.api.GalleyItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import retrofit2.http.Query
 import java.lang.Exception
@@ -15,6 +16,7 @@ private const val TAG = "PhotoGalleryViewModel"
 
 class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
+    private val preferenceRepository = PreferenceRepository.get()
 
     private val _galleryItems: MutableStateFlow<List<GalleyItem>> = MutableStateFlow(emptyList())
     val galleryItems: StateFlow<List<GalleyItem>>
@@ -22,20 +24,22 @@ class PhotoGalleryViewModel : ViewModel() {
 
     init {
         viewModelScope.launch {
-            try {
-                val items = fetchGalleryItems("planets")
-                _galleryItems.value = items
-            } catch (ex: Exception) {
-                Log.e(TAG, "Failed to fetch gallery items", ex)
+            preferenceRepository.storedQuery.collectLatest { storedQuery ->
+                try {
+                    val items = fetchGalleryItems(storedQuery)
+                    _galleryItems.value = items
+                } catch (ex: Exception) {
+                    Log.e(TAG, "Failed to fetch gallery items", ex)
+                }
             }
         }
     }
 
     fun setQuery(query: String) {
-        viewModelScope.launch { _galleryItems.value = fetchGalleryItems(query) }
+        viewModelScope.launch { preferenceRepository.setStoredQuery(query) }
     }
 
-    private suspend fun fetchGalleryItems(query: String) : List<GalleyItem> {
+    private suspend fun fetchGalleryItems(query: String): List<GalleyItem> {
         return if (query.isNotEmpty()) {
             photoRepository.searchPhotos(query)
         } else {
